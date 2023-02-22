7bda5b75e262b20aca8bb97d0511e37f5f94689b
Farishta7 <farishtatq@gmail.com>
2023-02-22 11:29:22 +0000

    spelling mistake for handlecustomerror corrected

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controller");
 const {getArticles, getArticleById} = require("./controllers/articles-controller");
-const {statusError500, handlePSQL400s, handleCustomerErrors} = require("./controllers/error-handling-controller")
+const {statusError500, handlePSQL400s, handleCustomErrors} = require("./controllers/error-handling-controller")
 
 app.use(express.json()); // first pice of middle-ware
 
@@ -14,7 +14,7 @@ app.get("/api/articles/:article_id", getArticleById)
 
 
 app.use(handlePSQL400s)
-app.use(handleCustomerErrors)
+app.use(handleCustomErrors)
 app.use(statusError500);
 
 module.exports = app;
