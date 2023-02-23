1d8fe9ccd4309f9bfadc4578cf820404cde41531
Farishta7 <farishtatq@gmail.com>
2023-02-23 11:13:26 +0000

    created fetchArticleCommentById

--- models/articles-model.js
+++ models/articles-model.js
@@ -21,3 +21,12 @@ exports.fetchArticleById = (article_id) => {
         
     })
 }
+
+exports.fetchArticleCommentById = (article_id) => {
+    return db
+    .query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments WHERE article_id = $1;`, [article_id])
+    .then((result) => {
+        //console.log(result.rows)
+        return result.rows
+    })
+}
