1c0f0a7ad9a629675a9032814b589ebc12fdb450
Farishta7 <farishtatq@gmail.com>
2023-02-23 14:32:18 +0000

    created another query to solve articles with no comments

--- models/articles-model.js
+++ models/articles-model.js
@@ -24,9 +24,19 @@ exports.fetchArticleById = (article_id) => {
 
 exports.fetchArticleCommentById = (article_id) => {
     return db
-    .query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments WHERE article_id = $1;`, [article_id])
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then((result) => {
-        //console.log(result.rows)
-        return result.rows
+        if (result.rows.length ===0) {
+            return Promise.reject({message: 'Article id not found.', status: 404})
+        } else {
+            return db
+            .query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body, comments.article_id FROM comments WHERE article_id = $1;`, [article_id])
+            .then((result) => {
+                    return result.rows
+                
+            })
+        }
     })
+
+    
 }
