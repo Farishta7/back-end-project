3f632c520a81b8c34be6c09b6e47b7912b875e6d
Farishta7 <farishtatq@gmail.com>
2023-02-24 10:21:32 +0000

    created post request and funtion: postArticleCommentById

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controller");
-const {getArticles, getArticleById, getArticleCommentById} = require("./controllers/articles-controller");
+const {getArticles, getArticleById, getArticleCommentById, postArticleCommentById} = require("./controllers/articles-controller");
 const {statusError500, handlePSQL400s, handleCustomErrors} = require("./controllers/error-handling-controller")
+
+app.use(express.json()); // first pice of middle-ware. used for post request. 
 
 app.get("/api/topics", getTopics);
 
@@ -12,6 +14,7 @@ app.get("/api/articles/:article_id", getArticleById)
 
 app.get("/api/articles/:article_id/comments", getArticleCommentById)
 
+app.post("/api/articles/:article_id/comments", postArticleCommentById)
 
 app.use(handlePSQL400s)
 app.use(handleCustomErrors)
